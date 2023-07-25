27ddbc3576651eae3e71938ced8bccc16d460192
saadazghour <saad.azghour@gmail.com>
2023-07-25 02:23:10 +0100

    feat: Add CustomModal to Home page for modal functionality.
    - Imported and used the CustomModal component to enable modal functionality on the Home page.
    - Added functionality to btn to trigger the opening of the modal when clicked.
    - Improved button styling for better user interaction.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+"use client";
+
 import CustomModal from "./components/CustomModal";
+import { useState } from "react";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <CustomModal />
-    </main>
+    <div className="flex flex-row items-center justify-center min-h-screen p-24">
+      <button
+        className="px-4 py-2 text-white bg-blue-500 rounded-md ml "
+        onClick={handleOpenModal}
+      >
+        Open Modal
+      </button>
+
+      <CustomModal
+        isOpen={isModalOpen}
+        onRequestClose={handleCloseModal}
+        contentLabel="My Modal Content"
+      />
+    </div>
   );
 }
